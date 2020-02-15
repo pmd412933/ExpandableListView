2c229adf1b0175e8e692ea0b239010d2062eb099
pmd412933 <pmd412933@gmail.com>
2020-02-15 11:09:12 +0600

    Expendable android stiduo

---
+++ app/src/main/java/com/example/expandable_list/ExpandableListDataPump.java
@@ -0,0 +1,74 @@
+package com.example.expandable_list;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class ExpandableListDataPump {
+    public static LinkedHashMap<String, List<String>> getData() {
+        LinkedHashMap<String, List<String>> expandableListDetail = new LinkedHashMap<>();
+
+        List<String> Kalima = new ArrayList<>();
+       Kalima.add("১. সমস্ত মুখমন্ডল কপালের উপরিভাগের চুলের গোড়া হইতে থুতনী পর্যন্ত\n" +
+               "এক কর্নের লতি থেকে অন্য কর্নের লতি পর্যন্ত ধৌত করা।\n" +
+               "\n" +
+               "২.উভয় হাত কনুইসহ ধৌত করা।\n" +
+               "\n" +
+               "৩.চারভাগের একভাগ মাথা মাসেহ করা \n" +
+               " ( ঘন দাঁড়ি থাকিলে আঙ্গুলী দ্বারা খেলাল করা ফরয )।\n" +
+               "\n" +
+               "৪.উভয় পা টাখনু গিরা সহকারে ধৌত করা ।\n");
+        List<String> sunnah = new ArrayList<>();
+       sunnah.add("1. নিয়ত করা\n" +
+               "2.বিসমিল্লাহ্ বলে ওযু শুরু করা।\n" +
+               "3.হাতের আঙ্গুল খিলাল করা।\n" +
+               "4.উভয় হাত কবজি পর্যন্ত ধৌত করা।\n" +
+               "5.মিসওয়াক করা।\n" +
+               "6.তিনবার কুলি করা।\n" +
+               "7.তিনবার নাকে পানি দেয়া।\n" +
+               "8.সম্পূর্ন মুখ মন্ডল তিনবার ধৌত করা।\n" +
+               "9.উভয় হাতের কনুইসহ তিনবার ধৌত করা।\n" +
+               "10.,সমস্ত মাথা একবার মাসেহ করা।\n" +
+               "11.,টাখনু সহ উভয় পা তিবার ধৌত করা।\n" +
+               "12.পায়ের আঙ্গুল খিলাল করা।\n" +
+               "13.এক অঙ্গ শুকানোর পূর্বে অন্য অঙ্গ ধৌত করা।\n" +
+               "14.ধারাবাহিকতা বজায় রেখে ওযুর কাজ গুলো সম্পূর্ন করা।\n");
+
+        List<String> Ozudua = new ArrayList<>();
+
+        Ozudua.add(
+                "বাংলা উচ্চারণঃ\n" +
+                        "\n" +
+                "আলহামদুলিল্লাহি আলা দ্বীন-ই ইসলাম\n" +
+                "আলইসলামু-হাক্কু, ওল কুফরে বাতীলুন\n" +
+                "ওল ইসলামু নূরুন, ওল কুফরে জুল্বমাত \n");
+        List<String> Others = new ArrayList<>();
+
+        Others.add("ওযু ভঙ্গের কারণ সমূহ :\n" +
+                "\n"+
+                "১.পায়খানা প্রস্রাবের রাস্তা দিয়া কোন কিছু বের হওয়া\n" +
+                "২. মুখ ভরে বমি হওয়া\n" +
+                "৩. শরীরের কোন জায়গা হতে রক্ত, পুঁজ বা পানি বের হয়ে গড়িয়ে পড়া\n" +
+                "৪. থুথুর সাথে রক্তের ভাগ সমান বা বেশি হওয়া\n" +
+                "৫. চিৎ বা কাত হয়ে হেলান দিয়ে ঘুম যাওয়া\n" +
+                "৬. পাগল, মাতাল, অচেতন হওয়া এবং\n" +
+                "৭. নামাযে উচ্চস্বরে হাসা।\n");
+        List<String> Taiamun = new ArrayList<>();
+
+        Taiamun.add("সর্বপ্রথম তায়াম্মুমের\n" +
+                "\n"+
+                "(১) নিয়ত করে নেবে,তারপর\n"+
+                "(২) উভয় হাত জমিনে উপর মেরে একবার সমস্ত মুখ মণ্ডল মুছে নিবে।\n" +
+                "(৩) পুনরায় জমিনে হাত মেরে উভয় হাত কনুই পর্যন্ত মুছবে।\n");
+
+
+        expandableListDetail.put("2.Sunnah", sunnah);
+        expandableListDetail.put("5.Others", Others);
+        expandableListDetail.put("1.Four Faraz in Ozu", Kalima);
+        expandableListDetail.put("4.Ozu er Dua", Ozudua);
+        expandableListDetail.put("3.Taiamun", Taiamun);
+
+        return expandableListDetail;
+    }
+}
---
+++ app/src/main/java/com/example/expandable_list/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.expandable_list;
+
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ViewUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    LinkedHashMap<String, List<String>> expandableListDetail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        Window g=getWindow();
+        g.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,WindowManager.LayoutParams.TYPE_STATUS_BAR);
+
+        setContentView(R.layout.activity_main);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int width = metrics.widthPixels;
+
+
+        expandableListView =  findViewById(R.id.expandableListView);
+        expandableListDetail = ExpandableListDataPump.getData();
+        expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
+        expandableListAdapter = new CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+
+            }
+        });
+
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                ).show();
+                return false;
+            }
+        });
+
+    }
+
+
+
+}
